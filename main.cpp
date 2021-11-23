dc24a973c75343ee80d315ddca584a20b4038c1e
dimiturtrz <dimitar.trz@gmail.com>
2021-11-23 14:30:14 +0200

    Update main.cpp
    adding a game loop

--- main.cpp
+++ main.cpp
@@ -4,43 +4,49 @@ using namespace std;
 
 int main() {
 	Field field;
+	bool gameLoopRunning = true;
 	char command;
-	cin >> command;
-	switch (command) {
-	case 'c':
+	while(gameLoopRunning) {
 		cin >> command;
 		switch (command) {
-		case 's':
-			Point origin;
-			double width, height;
-			Point ballStart;
-			double diameter;
-			cin >> origin;
-			cin >> width >> height;
-			cin >> ballStart;
-			cin >> diameter;
-			field = Field(origin, width, height, ballStart, diameter);
-			break;
 		case 'c':
-			Point endPoints[4];
-			Point ballStart;
-			double diameter;
-			cin >> ballStart;
-			cin >> diameter;
-			for (int i = 0; i < 4; ++i) {
-				cin >> endPoints[i];
+			cin >> command;
+			switch (command) {
+			case 's':
+				Point origin;
+				double width, height;
+				Point ballStart;
+				double diameter;
+				cin >> origin;
+				cin >> width >> height;
+				cin >> ballStart;
+				cin >> diameter;
+				field = Field(origin, width, height, ballStart, diameter);
+				break;
+			case 'c':
+				Point endPoints[4];
+				Point ballStart;
+				double diameter;
+				cin >> ballStart;
+				cin >> diameter;
+				for (int i = 0; i < 4; ++i) {
+					cin >> endPoints[i];
+				}
+				field = Field(endPoints, ballStart, diameter);
+				break;
 			}
-			field = Field(endPoints, ballStart, diameter);
+			break;
+		case 'h':
+			Point targetPoint;
+			double power;
+			cin >> targetPoint;
+			cin >> power;
+			field.hit(targetPoint, power);
+			break;
+		case 'q':
+			gameLoopRunning = false;
 			break;
 		}
-		break;
-	case 'h':
-		Point targetPoint;
-		double power;
-		cin >> targetPoint;
-		cin >> power;
-		field.hit(targetPoint, power);
-		break;
 	}
 
 	return 0;
