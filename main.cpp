762bb233640fd9be53a0a18e071713974a1e5c86
Dimitar <dimitar.trz@gmail.com>
2021-11-28 15:50:34 +0200

    added comments to the main

--- main.cpp
+++ main.cpp
@@ -5,13 +5,17 @@ using namespace std;
 int main() {
 	Field field;
 	bool gameLoopRunning = true;
+	// the value of command describes which option of the menu of the game is going to be used
 	char command;
 	while(gameLoopRunning) {
 		cin >> command;
 		switch (command) {
+        // 'c' stands for "change" - it's for changing the parameters of the field or the ball
 		case 'c':
 			cin >> command;
 			switch (command) {
+            // 's' stands for "simple case" - defines a field with an origin point, a height and a width, and a starting point for the ball.
+            // The horizontal side of the field is parallel to the x-axis, while the other one - to the y-axis
 			case 's':
 				Point origin;
 				double width, height;
@@ -23,6 +27,8 @@ int main() {
 				cin >> diameter;
 				field = Field(origin, width, height, ballStart, diameter);
 				break;
+            // 'c' stands for "complex case" - here the field is defined by four points and a starting point for the ball
+            // It is not nessecary for the sides of the field to be perpendicular to any axis
 			case 'c':
 				Point endPoints[4];
 				Point ballStart;
@@ -36,6 +42,7 @@ int main() {
 				break;
 			}
 			break;
+        // 'h' stands for "hit" - it's the option for defining in which direction and how strongly the ball is going to be hit
 		case 'h':
 			Point targetPoint;
 			double power;
@@ -43,6 +50,7 @@ int main() {
 			cin >> power;
 			field.hit(targetPoint, power);
 			break;
+        // 'q' stands for "quit" - it stops the game loop
 		case 'q':
 			gameLoopRunning = false;
 			break;
