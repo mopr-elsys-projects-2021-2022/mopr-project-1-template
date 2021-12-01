eb3a308653163bea189e2247bf99cf8acdec42cf
Momchil Todorov <mom.todorov.tues19@gmail.com>
2021-12-01 02:26:51 +0200

    fix comments

--- main.cpp
+++ main.cpp
@@ -9,17 +9,17 @@ int main() {
 		cout << "Enter a command (c - change parameters, h - hit ball, f - show field specs or q - quit): ";
 		cin >> command;
 		switch (command) {
-        	// 'c' stands for "change" - it's for changing the parameters of the field or the ball
+        		// 'c' stands for "change" - it's for changing the parameters of the field or the ball
 			case 'c':
 				cout << "Enter a command (s - simple field, c - complex field or b - ball): ";
 				cin >> command;
 				switch (command) {
-	            	// 's' stands for "simple case" - the horizontal side of the field is parallel to the x-axis, while the other one - to the y-axis
+	            			// 's' stands for "simple case" - the horizontal side of the field is parallel to the x-axis, while the other one - to the y-axis
 					case 's': 
 						cout << endl;
 						simpleCase(field);
 						break; 
-		            // 'c' stands for "complex case" - it is not nessecary for the sides of the field to be perpendicular to any axis
+		           		// 'c' stands for "complex case" - it is not nessecary for the sides of the field to be perpendicular to any axis
 					case 'c': 
 						cout << endl;
 						complexCase(field);
@@ -32,7 +32,7 @@ int main() {
 						cout << "Invalid command. Try again." << endl;
 				}
 				break;
-        	// 'h' stands for "hit" - it's the option for defining in which direction and how strongly the ball is going to be hit
+        		// 'h' stands for "hit" - it's the option for defining in which direction and how strongly the ball is going to be hit
 			case 'h': {
 				cout << endl;
 				Point targetPoint;
@@ -49,7 +49,7 @@ int main() {
 				cout << endl;
 				cout << field;
 				break;
-	        // 'q' stands for "quit" - it stops the game loop
+	        	// 'q' stands for "quit" - it stops the game loop
 			case 'q':
 				gameLoopRunning = false;
 				break;
